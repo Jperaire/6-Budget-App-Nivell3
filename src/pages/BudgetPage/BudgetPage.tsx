06c288d27cc25320ac8edd4463007ea162601986
Julen <julenperaire@gmail.com>
2025-06-17 12:20:49 +0200

    style: footer creado

--- src/pages/BudgetPage/BudgetPage.tsx
+++ src/pages/BudgetPage/BudgetPage.tsx
@@ -1,11 +1,13 @@
 import Header from "../../components/Header/Header.js";
 import BudgetForm from "../../components/BudgetForm/BudgetForm.js";
+import Footer from "../../components/Footer/Footer.js";
 
 const BudgetPage = () => {
     return (
         <>
             <Header />
             <BudgetForm />
+            <Footer />
         </>
     );
 };
